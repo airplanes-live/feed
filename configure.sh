4bbf211b2a26f1db925d05083e3debfada28336d
darken <darken@darken.eu>
2023-09-12 18:53:39 +0200

    Fix port collision with other feeders

--- configure.sh
+++ configure.sh
@@ -129,9 +129,9 @@ ALTITUDE="$RECEIVERALTITUDE"
 UAT_INPUT="127.0.0.1:30978"
 
 RESULTS="--results beast,connect,127.0.0.1:30104"
-RESULTS2="--results basestation,listen,31009"
+RESULTS2="--results basestation,listen,31015"
 RESULTS3="--results beast,listen,30157"
-RESULTS4="--results beast,connect,127.0.0.1:30169"
+RESULTS4="--results beast,connect,127.0.0.1:30187"
 # add --privacy between the quotes below to disable having the feed name shown on the mlat map
 # (position is never shown accurately no matter the settings)
 PRIVACY=""
@@ -139,7 +139,7 @@ INPUT_TYPE="$INPUT_TYPE"
 
 MLATSERVER="feed.airplanes.live:31090"
 TARGET="--net-connector feed.airplanes.live,30004,beast_reduce_plus_out,feed.airplanes.live,64004"
-NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30169 --net-bo-port 0 --net-ri-port 0 --write-json-every 1 --uuid-file /usr/local/share/airplanes/airplanes-uuid"
+NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30187 --net-bo-port 0 --net-ri-port 0 --write-json-every 1 --uuid-file /usr/local/share/airplanes/airplanes-uuid"
 JSON_OPTIONS="--max-range 450 --json-location-accuracy 2 --range-outline-hours 24"
 EOF
 
