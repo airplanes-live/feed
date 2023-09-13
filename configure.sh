584815fb613f6e204253cb2c350f37cc5c5e1be6
Husky <22480762+Huusky@users.noreply.github.com>
2023-09-12 23:00:29 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,7 +5,7 @@
 #####################################################################################
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #                                                                                   #
-# Copyright (c) 2020 ADSBx                                                          #
+# Copyright (c) 2023 AirDG                                                          #
 #                                                                                   #
 # Permission is hereby granted, free of charge, to any person obtaining a copy      #
 # of this software and associated documentation files (the "Software"), to deal     #
